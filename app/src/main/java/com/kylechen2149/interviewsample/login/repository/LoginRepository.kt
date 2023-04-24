e767d0262c8c3a1f300da94a0b05aef01379b683
Kyle <legolaslordofring@gmail.com>
2023-04-24 12:04:05 +0800

    feat(login): add login error method

--- app/src/main/java/com/kylechen2149/interviewsample/login/repository/LoginRepository.kt
+++ app/src/main/java/com/kylechen2149/interviewsample/login/repository/LoginRepository.kt
@@ -1,12 +1,27 @@
 package com.kylechen2149.interviewsample.login.repository
 
+import com.kylechen2149.interviewsample.InterviewSampleApp
 import com.kylechen2149.interviewsample.login.datasource.LoginService
 import com.kylechen2149.interviewsample.login.model.LoginRequest
 import com.kylechen2149.interviewsample.login.model.LoginResponse
+import com.kylechen2149.interviewsample.utils.HEADER_SESSION_TOKEN
+import com.kylechen2149.interviewsample.utils.KEY_NAME
+import com.kylechen2149.interviewsample.utils.KEY_OBJECT_ID
+import com.kylechen2149.interviewsample.utils.KEY_TIME_ZONE
+import retrofit2.Call
 
 class LoginRepository(private val loginService: LoginService) {
 
-    suspend fun login(username: String, password: String): LoginResponse {
+    fun login(username: String, password: String): Call<LoginResponse> {
         return loginService.login(LoginRequest(username, password))
+    }
+
+    fun saveData(sessionToken: String?, name: String?, timezone: String?, objectId: String?): Boolean {
+        return InterviewSampleApp.sharedPreferences.edit().apply {
+            putString(HEADER_SESSION_TOKEN, sessionToken)
+            putString(KEY_NAME, name)
+            putString(KEY_TIME_ZONE, timezone)
+            putString(KEY_OBJECT_ID, objectId)
+        }.commit()
     }
 }
